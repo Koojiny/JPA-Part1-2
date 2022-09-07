51c80b834175b90fca28aa17fd2489c7b0456e9d
Koojiny <msj09252@gmail.com>
2022-09-07 16:48:46 +0900

    test commit

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package jpabook.jpashop.repository;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+}
